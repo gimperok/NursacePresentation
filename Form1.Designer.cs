88da95e4c7e11502b2ee402837086c15dcfa9a0e
Alexei <gimp24@mail.ru>
2022-06-12 22:47:31 +0300

    Доработал функцию добавления записи в БД.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,21 @@
         private void InitializeComponent()
         {
             this.dgv = new System.Windows.Forms.DataGridView();
+            this.txt_Kod = new System.Windows.Forms.TextBox();
+            this.txt_Leather = new System.Windows.Forms.TextBox();
+            this.txt_Color = new System.Windows.Forms.TextBox();
+            this.txt_35 = new System.Windows.Forms.TextBox();
+            this.txt_36 = new System.Windows.Forms.TextBox();
+            this.txt_37 = new System.Windows.Forms.TextBox();
+            this.txt_38 = new System.Windows.Forms.TextBox();
+            this.txt_39 = new System.Windows.Forms.TextBox();
+            this.txt_40 = new System.Windows.Forms.TextBox();
+            this.txt_41 = new System.Windows.Forms.TextBox();
+            this.txt_Price = new System.Windows.Forms.TextBox();
+            this.txt_Note = new System.Windows.Forms.TextBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,26 +55,179 @@
             this.dgv.Location = new System.Drawing.Point(106, 55);
             this.dgv.Name = "dgv";
             this.dgv.ReadOnly = true;
-            this.dgv.Size = new System.Drawing.Size(998, 351);
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv.Size = new System.Drawing.Size(1124, 376);
             this.dgv.TabIndex = 0;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            // 
+            // txt_Kod
+            // 
+            this.txt_Kod.Location = new System.Drawing.Point(12, 489);
+            this.txt_Kod.Name = "txt_Kod";
+            this.txt_Kod.Size = new System.Drawing.Size(100, 20);
+            this.txt_Kod.TabIndex = 1;
+            // 
+            // txt_Leather
+            // 
+            this.txt_Leather.Location = new System.Drawing.Point(146, 489);
+            this.txt_Leather.Name = "txt_Leather";
+            this.txt_Leather.Size = new System.Drawing.Size(100, 20);
+            this.txt_Leather.TabIndex = 2;
+            // 
+            // txt_Color
+            // 
+            this.txt_Color.Location = new System.Drawing.Point(275, 489);
+            this.txt_Color.Name = "txt_Color";
+            this.txt_Color.Size = new System.Drawing.Size(100, 20);
+            this.txt_Color.TabIndex = 3;
+            // 
+            // txt_35
+            // 
+            this.txt_35.Location = new System.Drawing.Point(436, 489);
+            this.txt_35.Name = "txt_35";
+            this.txt_35.Size = new System.Drawing.Size(30, 20);
+            this.txt_35.TabIndex = 4;
+            this.txt_35.TextChanged += new System.EventHandler(this.txt_35_TextChanged);
+            // 
+            // txt_36
+            // 
+            this.txt_36.Location = new System.Drawing.Point(504, 489);
+            this.txt_36.Name = "txt_36";
+            this.txt_36.Size = new System.Drawing.Size(30, 20);
+            this.txt_36.TabIndex = 5;
+            this.txt_36.TextChanged += new System.EventHandler(this.txt_36_TextChanged);
+            // 
+            // txt_37
+            // 
+            this.txt_37.Location = new System.Drawing.Point(569, 489);
+            this.txt_37.Name = "txt_37";
+            this.txt_37.Size = new System.Drawing.Size(30, 20);
+            this.txt_37.TabIndex = 6;
+            this.txt_37.TextChanged += new System.EventHandler(this.txt_37_TextChanged);
+            // 
+            // txt_38
+            // 
+            this.txt_38.Location = new System.Drawing.Point(624, 489);
+            this.txt_38.Name = "txt_38";
+            this.txt_38.Size = new System.Drawing.Size(30, 20);
+            this.txt_38.TabIndex = 7;
+            this.txt_38.TextChanged += new System.EventHandler(this.txt_38_TextChanged);
+            // 
+            // txt_39
+            // 
+            this.txt_39.Location = new System.Drawing.Point(684, 489);
+            this.txt_39.Name = "txt_39";
+            this.txt_39.Size = new System.Drawing.Size(30, 20);
+            this.txt_39.TabIndex = 8;
+            this.txt_39.TextChanged += new System.EventHandler(this.txt_39_TextChanged);
+            // 
+            // txt_40
+            // 
+            this.txt_40.Location = new System.Drawing.Point(740, 489);
+            this.txt_40.Name = "txt_40";
+            this.txt_40.Size = new System.Drawing.Size(30, 20);
+            this.txt_40.TabIndex = 9;
+            this.txt_40.TextChanged += new System.EventHandler(this.txt_40_TextChanged);
+            // 
+            // txt_41
+            // 
+            this.txt_41.Location = new System.Drawing.Point(792, 489);
+            this.txt_41.Name = "txt_41";
+            this.txt_41.Size = new System.Drawing.Size(30, 20);
+            this.txt_41.TabIndex = 10;
+            this.txt_41.TextChanged += new System.EventHandler(this.txt_41_TextChanged);
+            // 
+            // txt_Price
+            // 
+            this.txt_Price.Location = new System.Drawing.Point(942, 489);
+            this.txt_Price.Name = "txt_Price";
+            this.txt_Price.Size = new System.Drawing.Size(40, 20);
+            this.txt_Price.TabIndex = 11;
+            this.txt_Price.TextChanged += new System.EventHandler(this.txt_Price_TextChanged);
+            // 
+            // txt_Note
+            // 
+            this.txt_Note.Location = new System.Drawing.Point(1073, 489);
+            this.txt_Note.Name = "txt_Note";
+            this.txt_Note.Size = new System.Drawing.Size(207, 20);
+            this.txt_Note.TabIndex = 13;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(907, 622);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 14;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(876, 489);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1008, 489);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1292, 687);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.txt_Note);
+            this.Controls.Add(this.txt_Price);
+            this.Controls.Add(this.txt_41);
+            this.Controls.Add(this.txt_40);
+            this.Controls.Add(this.txt_39);
+            this.Controls.Add(this.txt_38);
+            this.Controls.Add(this.txt_37);
+            this.Controls.Add(this.txt_36);
+            this.Controls.Add(this.txt_35);
+            this.Controls.Add(this.txt_Color);
+            this.Controls.Add(this.txt_Leather);
+            this.Controls.Add(this.txt_Kod);
             this.Controls.Add(this.dgv);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox txt_Kod;
+        private System.Windows.Forms.TextBox txt_Leather;
+        private System.Windows.Forms.TextBox txt_Color;
+        private System.Windows.Forms.TextBox txt_35;
+        private System.Windows.Forms.TextBox txt_36;
+        private System.Windows.Forms.TextBox txt_37;
+        private System.Windows.Forms.TextBox txt_38;
+        private System.Windows.Forms.TextBox txt_39;
+        private System.Windows.Forms.TextBox txt_40;
+        private System.Windows.Forms.TextBox txt_41;
+        private System.Windows.Forms.TextBox txt_Price;
+        private System.Windows.Forms.TextBox txt_Note;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,14 +13,20 @@ namespace FinalOrder
 {
     public partial class Form1 : Form
     {
-        DataBase database = new DataBase();
+        //DataBase database = new DataBase();
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=OrderDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public Form1()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateColumn();
+            RefreshDgv(dgv);
+        }
 
-        private void CreateColumn()
+        private void CreateColumn()  //создание колонок в DGV
         {
             dgv.Columns.Add("Id", "Id");
             dgv.Columns.Add("Kod", "Kod");
@@ -45,17 +51,24 @@ namespace FinalOrder
                          record.GetInt32(8), record.GetInt32(9), record.GetInt32(10), record.GetInt32(11),
                          record.GetDecimal(12), record.GetDecimal(13), record.GetString(14));
         }
-        private async void RefreshDgv(DataGridView dgv)
+
+        private void btn_Add_Click(object sender, EventArgs e) //кнопка добавления новой записи
+        {
+            AddNote();
+            RefreshDgv(dgv);
+        }
+
+        private async void RefreshDgv(DataGridView dgv) //обновление DGV
         {
             dgv.Rows.Clear();
 
-            using (database.getConnection())
+            using (SqlConnection connect = new SqlConnection(connectionString))
             {
-                database.openConnection();
+                await connect.OpenAsync();
 
                 string queryString = @"SELECT * FROM OrderTable";
 
-                SqlCommand command = new SqlCommand(queryString, database.getConnection());
+                SqlCommand command = new SqlCommand(queryString, connect);
                 SqlDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
@@ -65,11 +78,162 @@ namespace FinalOrder
                 reader.Close();
             }
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e) //вставка элементов в textbox'ы при клике по ячейке
         {
-            CreateColumn();
-            RefreshDgv(dgv);
+            if (e.RowIndex >= 0)
+            {
+
+               txt_Kod.Text = dgv[1, e.RowIndex].Value.ToString();
+               txt_Leather.Text = dgv[2, e.RowIndex].Value.ToString();
+               txt_Color.Text = dgv[3, e.RowIndex].Value.ToString();
+               txt_35.Text = dgv[4, e.RowIndex].Value.ToString();
+               txt_36.Text = dgv[5, e.RowIndex].Value.ToString();
+               txt_37.Text = dgv[6, e.RowIndex].Value.ToString();
+               txt_38.Text = dgv[7, e.RowIndex].Value.ToString();
+               txt_39.Text = dgv[8, e.RowIndex].Value.ToString();
+               txt_40.Text = dgv[9, e.RowIndex].Value.ToString();
+               txt_41.Text = dgv[10, e.RowIndex].Value.ToString();
+               txt_Price.Text = dgv[12, e.RowIndex].Value.ToString();
+               txt_Note.Text = dgv[14, e.RowIndex].Value.ToString();
+            }
+        }
+
+
+        private void AddNote() //запрос к бд на ДОБАВЛЕНИЕ данных 
+        {
+            using (SqlConnection connect = new SqlConnection(connectionString))
+            {
+                connect.Open();
+
+                var kod = txt_Kod.Text.ToUpper();
+                var leather = txt_Leather.Text.ToUpper();
+                var color = txt_Color.Text.ToUpper();
+                int txt35, txt36, txt37, txt38, txt39, txt40, txt41, txtKol;
+                decimal txtPrice, txtTPrice;
+                var txtNote = txt_Note.Text;
+
+                if (!string.IsNullOrEmpty(kod) && !string.IsNullOrEmpty(leather) && !string.IsNullOrEmpty(color))
+                {
+                    if (int.TryParse(txt_35.Text, out txt35) && int.TryParse(txt_36.Text, out txt36) && int.TryParse(txt_37.Text, out txt37)
+                        && int.TryParse(txt_38.Text, out txt38) && int.TryParse(txt_39.Text, out txt39) && int.TryParse(txt_40.Text, out txt40)
+                        && int.TryParse(txt_41.Text, out txt41) && decimal.TryParse(txt_Price.Text, out txtPrice))
+                    {
+                        txtKol = txt35 + txt36 + txt37 + txt38 + txt39 + txt40 + txt41;
+                        label1.Text = txtKol.ToString();
+
+                        txtTPrice = Convert.ToDecimal(label2.Text);
+
+
+                        var addQuery = "INSERT INTO OrderTable (Kod, Leather, Color, s35, s36, s37, s38, s39, s40, s41, Total, Price, TPrice, Note) " +
+                                                  "VALUES (@kod, @leather, @color, @txt35, @txt36, @txt37, @txt38, @txt39, @txt40, @txt41, @txtKol, " +
+                                                          "@txtPrice, @txtTPrice, @txtNote)";
+
+                        SqlCommand command = new SqlCommand(addQuery, connect);
+
+                        command.Parameters.AddWithValue("@kod", kod);
+                        command.Parameters.AddWithValue("@leather", leather);
+                        command.Parameters.AddWithValue("@color", color);
+                        command.Parameters.AddWithValue("@txt35", txt35);
+                        command.Parameters.AddWithValue("@txt36", txt36);
+                        command.Parameters.AddWithValue("@txt37", txt37);
+                        command.Parameters.AddWithValue("@txt38", txt38);
+                        command.Parameters.AddWithValue("@txt39", txt39);
+                        command.Parameters.AddWithValue("@txt40", txt40);
+                        command.Parameters.AddWithValue("@txt41", txt41);
+                        command.Parameters.AddWithValue("@txtKol", txtKol);
+                        command.Parameters.AddWithValue("@txtPrice", txtPrice);
+                        command.Parameters.AddWithValue("@txtTPrice", txtTPrice);
+                        command.Parameters.AddWithValue("@txtNote", txtNote);
+                        command.ExecuteNonQuery();
+
+                        ClearTextBox();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                connect.Close();
+            }
+        }
+
+        private void ClearTextBox()
+        {
+            txt_Kod.Clear();
+            txt_Leather.Clear();
+            txt_Color.Clear();
+            txt_35.Clear();
+            txt_36.Clear();
+            txt_37.Clear();
+            txt_38.Clear();
+            txt_39.Clear();
+            txt_40.Clear();
+            txt_41.Clear();
+            txt_Price.Clear();
+            txt_Note.Clear();
+        }
+
+        //отвечает за работу label1 и label2
+        private void resLabel()
+        {
+            int a, b, c, d, e, f, g;
+            int.TryParse(txt_35.Text, out a);
+            int.TryParse(txt_36.Text, out b);
+            int.TryParse(txt_37.Text, out c);
+            int.TryParse(txt_38.Text, out d);
+            int.TryParse(txt_39.Text, out e);
+            int.TryParse(txt_40.Text, out f);
+            int.TryParse(txt_41.Text, out g);
+            int res = a + b + c + d + e + f + g;
+
+            label1.Text = Convert.ToString(res);
+
+            decimal x, y;
+            decimal.TryParse(txt_Price.Text, out x);
+            decimal.TryParse(label1.Text, out y);
+            decimal res1 = x * y;
+
+            label2.Text = Convert.ToString(res1);
+        }
+        private void txt_35_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
+        }
+
+        private void txt_36_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
+        }
+
+        private void txt_37_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
+        }
+
+        private void txt_38_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
+        }
+
+        private void txt_39_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
+        }
+
+        private void txt_40_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
+        }
+
+        private void txt_41_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
+        }
+
+        private void txt_Price_TextChanged(object sender, EventArgs e)
+        {
+            resLabel();
         }
     }
 }
