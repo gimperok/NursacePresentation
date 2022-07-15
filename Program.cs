7a3d7fee08e3390f6a9159680dc31401b6308c7d
Cojevnicov Alexei <89630838+gimperok@users.noreply.github.com>
2022-07-15 11:15:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace FinalOrder
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
